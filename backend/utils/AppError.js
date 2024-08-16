c3cfda84a570c48e705ce04289dad2a200b65f6b
Renuka Sinha <148695148+Rskri2@users.noreply.github.com>
2024-08-16 21:13:50 +0530

    Update AppError.js

--- backend/utils/AppError.js
+++ backend/utils/AppError.js
@@ -6,6 +6,7 @@ class AppError extends Error{
         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
         this.isOperational = `${statusCode}`.startsWith('4') ? true : false;
        Error.captureStackTrace(this,this.constructor);
+        
     }
 }
 
